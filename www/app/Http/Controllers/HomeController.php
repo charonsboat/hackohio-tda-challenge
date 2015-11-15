0b6decaa218aae7525cabcef57336f0282791786
hornbd <hornbd96@gmail.com>
2015-11-14 23:16:28 -0500

    Your welcome

--- www/app/Http/Controllers/HomeController.php
+++ www/app/Http/Controllers/HomeController.php
@@ -15,6 +15,34 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $practice_string_array = ['hello','Goodbye!','goodbye','no','yes'];
+
+        $array = array();
+        for ($i = 0; $i <= ((count($practice_string_array)) -1) ; $i++) {
+            $practice_string_array[$i] = strtolower($practice_string_array[$i]);
+            $practice_string_array[$i] = trim( preg_replace( "/[^0-9a-z]+/i", " ", $practice_string_array[$i] ) );
+            if (array_key_exists(strtolower($practice_string_array[$i]), $array)) {
+                $array[$practice_string_array[$i]] += 1;
+            } else {
+                $array[$practice_string_array[$i]] = 1;
+            }
+        }
+        $my_size = count($array);
+        echo('<br>');
+        echo("Size: ");
+        echo($my_size);
+        echo('<br>');
+        echo('<br>');
+        foreach ($array as $key => $value){
+            echo($key);
+            echo(' :: ');
+            echo($value);
+            echo('<br>');
+        }
+
+        echo('<br>');
+        echo('<br>');
+        echo("Ben is doing work");
         return view('home');
     }
 }
